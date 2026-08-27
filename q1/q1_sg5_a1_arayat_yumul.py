91fdd41021a8c7c28bc282de91cc8255b3c002d7
Raiden-Yumul-2030 <rlyumul2030@clc.pshs.edu.ph>
2026-08-27 19:32:48 +0800

    code more

--- q1/q1_sg5_a1_arayat_yumul.py
+++ q1/q1_sg5_a1_arayat_yumul.py
@@ -1,20 +1,19 @@
 class Hero:
     def __init__(hero, health, name):
-        health = 100
         hero.health = health
         hero.name = name
-    def damage_a(hero, health, name, dmg_a):
-        hero.name = "Arthur"
-        hero.health -= dmg_a
-        print(f"{hero.name} took {dmg} damage, and now has {hero.health} health.")
-    def damage_m(hero, health, name, dmg_m):
-        hero.name = "Morgana"
-        hero.health -= dmg_m
-        print(f"{hero.name} took {dmg} damage, and now has {hero.health} health.")
+    def damage(hero, dmg):
+        hero.health -= dmg
+        print(f"{hero.name} took {dmg} dmg and now has {hero.health} health left")
 
-dmg_a = 10
-dmg_m = 0
-heros = Hero(health, name, dmg_a, dmg_m)
-heros.damage_a(health, name, dmg_a)
-heros.damage_m(health, name, dmg_m)
+Names = ["Arthur", "Morgana"]
+Hero_Damage = [0, 10]
+Hero_Health = [100, 100]
+
+for i in range(len(names)):
+    health = Hero_Health[i]
+    name = Names[i]
+    dmg = Hero_Damage[i]
+    heros = Hero(health, name)
+    heros.damage(dmg)
 
