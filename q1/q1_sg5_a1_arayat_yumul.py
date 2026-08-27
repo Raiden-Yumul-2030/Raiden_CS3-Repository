4dba158af91a81bfdd3e7062b12f1f7f9757e6f6
Raiden-Yumul-2030 <rlyumul2030@clc.pshs.edu.ph>
2026-08-27 19:33:09 +0800

    syntax fixed

--- q1/q1_sg5_a1_arayat_yumul.py
+++ q1/q1_sg5_a1_arayat_yumul.py
@@ -7,10 +7,10 @@ class Hero:
         print(f"{hero.name} took {dmg} dmg and now has {hero.health} health left")
 
 Names = ["Arthur", "Morgana"]
-Hero_Damage = [0, 10]
+Hero_Damage = [10, 0]
 Hero_Health = [100, 100]
 
-for i in range(len(names)):
+for i in range(len(Names)):
     health = Hero_Health[i]
     name = Names[i]
     dmg = Hero_Damage[i]
