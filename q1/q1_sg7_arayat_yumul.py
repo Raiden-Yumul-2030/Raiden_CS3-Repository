5bec1d816838f436efaad9a43426a51b6e1d7f05
Raiden-Yumul-2030 <rlyumul2030@clc.pshs.edu.ph>
2026-09-10 11:44:57 +0800

    idk

--- q1/q1_sg7_arayat_yumul.py
+++ q1/q1_sg7_arayat_yumul.py
@@ -1,1 +1,16 @@
-class
+class glassware:
+    def __init__(self, glassware_type):
+        self.glassware_type = glassware_type
+    def inspect(self):
+        print(f"This glassware is called {self.glassware_type}")
+
+class beaker(glassware):
+    def __init__(self, beakers):
+        super().__init__(glassware_type = "beaker")
+        self.beakers = beakers
+    def count(self):
+        print(f"There is a {self.beakers} here")
+
+beaker = beaker(glassware)
+beaker.count()
+beaker.inspect()
