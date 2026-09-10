9910c3bfcc66e8ca4168d6753d838da5e166ae50
Raiden-Yumul-2030 <rlyumul2030@clc.pshs.edu.ph>
2026-09-10 12:22:20 +0800

    confusing thingy kinda

--- q1/q1_sg7_arayat_yumul.py
+++ q1/q1_sg7_arayat_yumul.py
@@ -1,21 +1,25 @@
 class glassware:
     def __init__(self, glassware_type):
         self.glassware_type = glassware_type
-
-class beaker(glassware):
-    def __init__(self, beakers, glassware_type = "beaker"):
-        super().__init__(glassware_type)
-        self.beakers = []
     def inspect(self):
             print(f"This glassware is called {self.glassware_type}")
-    def count(self):
-        print(f"There are {self.beakers} here")
 
-class tray(beaker):
-    def __init__(self, beaker_amount):
-        super().__init__(beakers)
-        self.beaker_amount = len(self.beakers)
+class beaker(glassware):
+    def __init__(self, glassware_type):
+        super().__init__(glassware_type)
+        self.glassware_type = "beaker"
+
+class tray:
+    def __init__(self, beakers):
+        self.trayinventory = []
+    def beaker_add(self):
+        if len(self.trayinventory) < 5:
+            self.trayinventory.append()
+        else:
+            print("Too many beakers on tray already!")
+    def beakers(self):
+        print(f"The beaker capacity of this tray is {len(self.trayinventory)}/5")
 
 beaker = beaker("beaker")
 beaker.inspect()
-beaker.count()
+tray = tray("tray")
