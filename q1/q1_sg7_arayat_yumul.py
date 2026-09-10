6b440f9b922fd97293fe3755da435f53bbc073ac
Raiden-Yumul-2030 <rlyumul2030@clc.pshs.edu.ph>
2026-09-10 12:00:53 +0800

    ...

--- q1/q1_sg7_arayat_yumul.py
+++ q1/q1_sg7_arayat_yumul.py
@@ -1,16 +1,21 @@
 class glassware:
     def __init__(self, glassware_type):
         self.glassware_type = glassware_type
-    def inspect(self):
-        print(f"This glassware is called {self.glassware_type}")
 
 class beaker(glassware):
-    def __init__(self, beakers):
-        super().__init__(glassware_type = "beaker")
-        self.beakers = beakers
+    def __init__(self, beakers, glassware_type = "beaker"):
+        super().__init__(glassware_type)
+        self.beakers = []
+    def inspect(self):
+            print(f"This glassware is called {self.glassware_type}")
     def count(self):
-        print(f"There is a {self.beakers} here")
+        print(f"There are {self.beakers} here")
 
-beaker = beaker(glassware)
-beaker.count()
+class tray(beaker):
+    def __init__(self, beaker_amount):
+        super().__init__(beakers)
+        self.beaker_amount = len(self.beakers)
+
+beaker = beaker("beaker")
 beaker.inspect()
+beaker.count()
